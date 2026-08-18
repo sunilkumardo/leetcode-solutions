49ef20ae21d29dd7fade971d49d2ae5584367983
SUNIL KUMAR D O <122513479+sunilkumardo@users.noreply.github.com>
2026-08-18 23:51:34 +0530

    Time: 0 ms (100%), Space: 47.4 MB (49.77%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,16 +1,11 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n=nums.length;
-        int numsum=0;
-        int incsum=0;
-        int result=0;
+        int xor =n;
         for(int i=0;i<n;i++){
-            numsum +=nums[i];
-            incsum +=i;
-
+            xor ^=i;
+            xor ^=nums[i];
         }
-        incsum +=n;
-        result=incsum - numsum;
-        return result;
+        return xor;
     }
 }
